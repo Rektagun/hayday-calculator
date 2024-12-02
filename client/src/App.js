8a1ae68e658422f27219e1be69b0276d5fceec74
Rektagun <khatrisurya.101202@gmail.com>
2024-12-02 18:29:53 +0530

    Adding some data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import ItemCard from './comps/itemCard';
+import Silo from './comps/silo';
 import './output.css';
+
+
+
+
+
+
 
 function App() {
 
   return (
 
-    <div className='m-2 bg-blue-50 h-screen'>
+    <div className='mx-auto bg-blue-50 h-screen w-3/5'>
 
       <div className='flex flex-col w-full'>
 
@@ -17,31 +24,34 @@ function App() {
 
           <div className='flex flex-col w-full'>
 
-            <div className='flex flex-row justify-center'> 
-              <img className='size-12' src='./icons/Silo.webp' alt='barn' />
-            <h1 className='text-center font-bold text-4xl'>
-              silo
-            </h1>
-            </div>
+            <Silo/>
 
             <div className='flex flex-row flex-wrap'>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
-              <ItemCard/>
+
+
+              <button className='flex flex-row h-20 bg-white w-fit rounded-xl m-1
+                border-2 border-gray-600 justify-between shadow-black shadow-sm 
+                hover:shadow-md hover:shadow-blue-900 group'>
+
+                <img className='size-16 absolute z-10 blur-sm self-center ml-1 rounded-full' 
+                  src='./icons/Corn.webp' alt='Spicy_Pizza' />
+                <img className='size-16 self-center z-20 ml-1 rounded-lg' 
+                  src='./icons/Corn.webp' alt='Spicy_Pizza' />
+
+                <div className='flex flex-col m-1 p-1 rounded-lg justify-start 
+                  border-black border bg-blue-100 text-center'>
+                  <h1 className='font-extrabold border-2 border-black rounded-lg bg-white 
+                    text-center'>3</h1>
+                  <h1 className='text-gray-700 text-sm rounded-lg group-hover:hidden'>Spicy Pizza</h1>
+                  <h1 className='text-blue-900 text-xs rounded-lg group-hover:hidden'>Bakery</h1>
+                  <button className='font-extrabold border-2 border-black rounded-lg 
+                    bg-white text-center hidden group-hover:block'>
+                    ⊖
+                  </button>
+                </div>
+
+
+              </button>
             </div>
 
           </div>
