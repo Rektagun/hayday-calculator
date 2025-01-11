f15d11b6597ca1d651d6adcba70cd4dc2c503f36
Rektagun <khatrisurya.101202@gmail.com>
2025-01-12 02:23:36 +0530

    Neo4j and right command

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-
-const A_URI = process.env.AURAURI
-const A_PASS = process.env.AURAPASSWORD
-const A_USER = process.env.AURAUSER
-
 var neo4j = require('neo4j-driver');
 
 (async () => {
 
-  // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
-  const URI = A_URI;
-  const USER = A_USER;
-  const PASSWORD = A_PASS;
+  const URI='neo4j+s://afe81a2a.databases.neo4j.io'
+  const USER='neo4j'
+  const PASSWORD='VcKQNmXMCW3T2OR_-URhFumMDoFrQWxqnB1LkVfOHEc'
 
   let driver
 
   try {
+
     driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
-    const serverInfo = await driver.getServerInfo()
-    console.log('Connection established')
-    console.log(serverInfo)
+    console.log(await driver.executeQuery('MATCH (n) RETURN n LIMIT 1'));
 
+    const user_level = 5;
 
+    // Variables:
+    // 0. User's exp level in the game.
+    //    - Only show those items.
+    // 1. What items to fetch?
+    //    - Some other info about it?
+    
+    // TODO [ RECIEVE DATA FROM FRONTEND ]
+
+    // TODO [ CLEAN DATA AND MAKE IT USEFULL ]
+
+    // Que. What data to request?
+    // Ans. Ask the user for items needed and then get back with that info.
+    //    - Mostly everything in the database, but only required items.
+    //    - Remove stupid things from the results.
+    //    - Save that data nicely to send to the frontend.
+
+    // TODO [ SEND DATA TO FRONTEND ]
+
+    driver.close();
 
   } catch(err) {
     console.log(`Connection error\n${err}\nCause: ${err.cause}`)
--- useful.js
+++
@@ -1,2 +0,0 @@
-item.minPrice = ${itemData.priceRange?.min || 0},
-item.maxPrice = ${itemData.priceRange?.max || 0}`);
